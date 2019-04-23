de3c431e9285df9096f46d3bc70adc29fa777d98
Ngo Phuc Cuong <41280374+phuccuongngo99@users.noreply.github.com>
2019-04-23 08:56:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fgan_train import training_pipeline
 parser = argparse.ArgumentParser('Train your Fence GAN')
 
 ###Training hyperparameter
-args = parser.add_argument('--dataset', type=str, default='mnist', help='2d | kdd99 | mnist | cifar10')
+args = parser.add_argument('--dataset', type=str, default='mnist', help='mnist | cifar10')
 args = parser.add_argument('--ano_class',type=int, default=2, help='1 anomaly class')
 args = parser.add_argument('--epochs', type=int, default=100, help='number of epochs to train')
 
@@ -21,7 +21,7 @@ args = parser.add_argument('--d_lr', type=float, default=1e-5, help='learning_ra
 args = parser.add_argument('--g_lr', type=float, default=2e-5, help='learning rate of generator')
 args = parser.add_argument('--v_freq', type=int, default=4, help='epoch frequency to evaluate performance')
 args = parser.add_argument('--seed', type=int, default=0, help='numpy and tensorflow seed')
-args = parser.add_argument('--evaluation', type=str, default='auprc', help='AUPRC or AUROC')
+args = parser.add_argument('--evaluation', type=str, default='auprc', help="'auprc' or 'auroc'")
 args = parser.add_argument('--latent_dim', type=int, default=200, help='Latent dimension of Gaussian noise input to Generator')
 
 args = parser.parse_args()
